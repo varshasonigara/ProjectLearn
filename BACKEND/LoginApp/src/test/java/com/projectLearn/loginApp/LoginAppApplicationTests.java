896a5bbdb48c5a653547bfcf1ec7ae7beabf6877
varshasonigara <varshasonigara2017@gmail.com>
2020-01-23 21:51:29 +0530

    Add Backend

---
+++ BACKEND/LoginApp/src/test/java/com/projectLearn/loginApp/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectLearn.loginApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
